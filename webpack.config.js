d6cee874f9cc306e66549ccd51be7604d2883678
Soro Cat <sorousha326@gmail.com>
2020-04-13 12:38:11 -0700

    add changes

---
+++ webpack.config.js
@@ -0,0 +1,5 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
