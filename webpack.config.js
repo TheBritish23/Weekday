3c478edce3f5f3cc80ee80369937f03c09495493
Soro Cat <sorousha326@gmail.com>
2020-04-14 09:41:32 -0700

    add changes

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     new UglifyJsPlugin({ sourceMap: true }),
      new CleanWebpackPlugin(),
      new HtmlWebpackPlugin({
-       title: 'T-angle',
+       title: 'Weekday',
        template: './src/Index.html',
        inject: 'body'
      })
