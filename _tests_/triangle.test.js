d84340ea93c8dbb5af23204650de82bd2a21ca91
Soro Cat <sorousha326@gmail.com>
2020-04-21 12:07:56 -0700

    add changes

--- _tests_/triangle.test.js
+++ _tests_/triangle.test.js
@@ -3,6 +3,7 @@ import { Triangle } from './../src/triangle.js';
 describe('Triangle', () => {
 
 var triangle = new Triangle(2,4,5);
+expect(value1).toEqual(value2);
 expect(triangle.side1).toEqual(2);
 expect(triangle.side2).toEqual(4);
 expect(triangle.side3).toEqual(5);
